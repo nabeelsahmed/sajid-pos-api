31d0d593818486db1bb91985bc4b3ae56f9df4e6
HammadHere12 <hammadnoor120@gmail.com>
2022-12-14 11:42:49 +0500

    changes in RoleController

--- UMISModuleApi/Controllers/RoleController.cs
+++ UMISModuleApi/Controllers/RoleController.cs
@@ -241,6 +241,206 @@ namespace UMISModuleApi.Controllers
             }
 
         }
+
+        [HttpPost("updateRole")]
+        public IActionResult updateRole(RoleCreation obj)
+        {
+            try{
+                DateTime curDate = DateTime.Today;
+
+                DateTime curTime = DateTime.Now;
+                
+                var time = curTime.ToString("HH:mm");
+
+                int rowAffected = 0;
+                int rowAffected2 = 0;
+                var response = "";
+                var found = false;
+                var roleTitle = "";
+                var newRoleID = 0;
+                var newRoleDetailID = 0;
+
+                List<RoleCreation> appMenuUser = new List<RoleCreation>();
+                cmd = "select \"roleTitle\" from roles where \"roleTitle\"='" + obj.roleTitle + "'";
+                appMenuUser = (List<RoleCreation>)dapperQuery.Qry<RoleCreation>(cmd, _dbCon);
+
+                List<RoleCreation> appMenuRoleID = new List<RoleCreation>();
+                cmd3 = "select \"roleID\" from roles ORDER BY \"roleID\" DESC LIMIT 1";
+                appMenuRoleID = (List<RoleCreation>)dapperQuery.Qry<RoleCreation>(cmd3, _dbCon);
+
+                
+                
+                if(appMenuRoleID.Count == 0)
+                    {
+                        newRoleID = 1;    
+                    }else{
+                        newRoleID = appMenuRoleID[0].roleID+1;
+                    }
+
+                if (appMenuUser.Count > 0)
+                        roleTitle = appMenuUser[0].roleTitle;
+
+                
+                if(obj.roleID >= 1)
+                {   
+
+                    cmd2 = "update public.\"roles\" set  \"roleTitle\" = '" + obj.roleTitle + "', \"roleDescription\" = '" + obj.roleDescription + "' , \"modifiedOn\" = '" + curDate + "', \"modifiedBy\" = " + obj.userID + " where \"roleID\" = " + obj.roleID + ";";
+                    
+                }
+                else
+                {
+                    found=true;
+                }
+                if (found == false)
+                {
+                    using (NpgsqlConnection con = new NpgsqlConnection(_dbCon.Value.dbCon))
+                    {
+                        rowAffected = con.Execute(cmd2);
+                    }
+                    
+                }
+
+                if (rowAffected > 0 )
+                {   
+
+                    cmd5 = "update public.\"roles_detail\" set \"isDeleted\" = 1 where \"roleId\" = " + obj.roleID + ";";
+                    using (NpgsqlConnection con = new NpgsqlConnection(_dbCon.Value.dbCon))
+                    {
+                        rowAffected = con.Execute(cmd5);
+                    }
+
+                    var invObject = JsonConvert.DeserializeObject<List<RoleDetailCreation>>(obj.json);
+
+                    foreach (var item in invObject)
+                    {
+                        List<RoleDetailCreation> appMenuRoleDetailID = new List<RoleDetailCreation>();
+                        cmd4 = "select \"roleDetailId\" from roles_detail ORDER BY \"roleDetailId\" DESC LIMIT 1";
+                        appMenuRoleDetailID = (List<RoleDetailCreation>)dapperQuery.Qry<RoleDetailCreation>(cmd4, _dbCon);
+
+                        List<RoleDetailCreation> appMenuRoleMenuDetail = new List<RoleDetailCreation>();
+                        cmd6 = "select \"roleDetailId\" from roles_detail where \"roleId\" = " + obj.roleID + " and \"menuId\" = " + item.menuId + " ";
+                        appMenuRoleMenuDetail = (List<RoleDetailCreation>)dapperQuery.Qry<RoleDetailCreation>(cmd4, _dbCon);
+                        
+                        if (appMenuRoleMenuDetail.Count == 0)
+                        {
+                            if(appMenuRoleDetailID.Count == 0)
+                            {
+                                newRoleDetailID = 1;    
+                            }else{
+                                newRoleDetailID = appMenuRoleDetailID[0].roleDetailID+1;
+                            }
+
+                            cmd6 = "insert into public.\"roles_detail\" (\"roleDetailId\", \"menuId\", \"roleId\", \"createdOn\", \"createdBy\", \"isDeleted\", \"read\", \"write\", \"delete\") values (" + newRoleDetailID + ", '" + item.menuId + "', " + newRoleID + ", '" + curDate + "', " + obj.userID + ", 0," + item.read + "," + item.write + "," + item.delete + ")";
+                            using (NpgsqlConnection con = new NpgsqlConnection(_dbCon.Value.dbCon))
+                            {
+                                rowAffected2 = con.Execute(cmd6);
+                            }
+                        }
+                        else
+                        {
+                            cmd = "update public.\"roles_detail\" set \"isDeleted\" = 0 where \"roleId\" = " + obj.roleID + " and \"menuId\" = " + item.menuId + " ";
+                            using (NpgsqlConnection con = new NpgsqlConnection(_dbCon.Value.dbCon))
+                            {
+                                rowAffected2 = con.Execute(cmd);
+                            }
+                        }
+                     
+                    }
+                    
+
+                }
+                if(rowAffected > 0 )
+                {
+                    response = "Success";
+                    return Ok(new { message = response });
+                }
+                else
+                {
+                    if (found == true)
+                    {
+                        response = "Role not exist";
+                    }
+                    else
+                    {
+                        response = "Server Issue";
+                    }
+                return BadRequest(new { message = response });
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                return Ok(e);
+            }
+
+        }
+
+        [HttpPost("deleteRole")]
+        public IActionResult deleteRole(RoleCreation obj)
+        {
+            try{
+                DateTime curDate = DateTime.Today;
+
+                DateTime curTime = DateTime.Now;
+                
+                var time = curTime.ToString("HH:mm");
+
+                int rowAffected = 0;
+                int rowAffected2 = 0;
+                var response = "";
+                var found = false;
+
+                
+                if(obj.roleID >= 1)
+                {   
+
+                    cmd2 = "update public.\"roles\" set  \"roleTitle\" = '" + obj.roleTitle + "', \"roleDescription\" = '" + obj.roleDescription + "' , \"modifiedOn\" = '" + curDate + "', \"modifiedBy\" = " + obj.userID + " where \"roleID\" = " + obj.roleID + ";";
+                    cmd5 = "update public.\"roles_detail\" set \"isDeleted\" = 1 where \"roleId\" = " + obj.roleID + ";";
+                }
+                else
+                {
+                    found=true;
+                }
+                if (found == false)
+                {
+                    using (NpgsqlConnection con = new NpgsqlConnection(_dbCon.Value.dbCon))
+                    {
+                        rowAffected = con.Execute(cmd2);
+                    }
+                    
+                    using (NpgsqlConnection con = new NpgsqlConnection(_dbCon.Value.dbCon))
+                    {
+                        rowAffected2 = con.Execute(cmd5);
+                    }
+                }
+                
+                if(rowAffected > 0 && rowAffected2 > 0)
+                {
+                    response = "Success";
+                    return Ok(new { message = response });
+                }
+                else
+                {
+                    if (found == true)
+                    {
+                        response = "Role not exist";
+                    }
+                    else
+                    {
+                        response = "Server Issue";
+                    }
+                return BadRequest(new { message = response });
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                return Ok(e);
+            }
+
+        }
         
     }
 }
